9d32ab7e89f1ddb55be9483f0b0278b0e4553c7f
jdm79 <james.malvern@gmail.com>
2020-03-31 21:00:45 +0100

    hooks up the slider to a chart which is reactive

--- input.R
+++ input.R
@@ -1,14 +1,20 @@
 library(shiny)
 
 ui <- fluidPage(
-  sliderInput(inputId = "num",
-      label = "Choose a number",
-      value = 25, min = 1, max = 100
-              )
+  
+  sliderInput(
+    inputId = "num",
+    label = "Choose a number",
+    value = 25, min = 1, max = 100
+  ),
+  plotOutput("hist")
+  
 )
 
 server <- function(input, output, session) {
-  
+  output$hist <- renderPlot({
+    hist(rnorm(input$num))
+  })
 }
 
 shinyApp(ui = ui, server = server)
